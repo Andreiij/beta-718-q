0cf2f977d795ce1dfb975d1d362b8f9c04ebfe00
Andreiij <andrei.gher@yahoo.com>
2022-06-23 16:07:55 +0100

    Added initial project plan

--- script.ps1
+++ script.ps1
@@ -2,3 +2,4 @@ Initial content for script.ps1
 Temporary commit content for 2022-06-21T15:52:35
 Temporary commit content for 2022-06-30T15:52:35
 Temporary commit content for 2022-07-08T15:52:35
+Temporary commit content for 2022-06-23T16:07:55

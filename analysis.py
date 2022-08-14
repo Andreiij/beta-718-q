e3843eeaef01234336b5e27d959b90a3a6bee858
Andreiij <andrei.gher@yahoo.com>
2022-08-14 16:07:55 +0100

    Fixed bug in data processing script

--- analysis.py
+++ analysis.py
@@ -4,3 +4,4 @@ Temporary commit content for 2022-06-27T15:52:35
 Temporary commit content for 2022-06-29T15:52:35
 Temporary commit content for 2022-07-10T15:52:35
 Temporary commit content for 2022-08-06T16:07:55
+Temporary commit content for 2022-08-14T16:07:55

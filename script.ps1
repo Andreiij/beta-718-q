c23f6988b447c699a7e9a06512627d9734d1f176
Andreiij <andrei.gher@yahoo.com>
2022-10-15 16:07:55 +0100

    Fixed typos in documentation

--- script.ps1
+++ script.ps1
@@ -11,3 +11,4 @@ Temporary commit content for 2022-07-26T16:07:55
 Temporary commit content for 2022-08-17T16:07:55
 Temporary commit content for 2022-09-14T16:07:55
 Temporary commit content for 2022-09-27T16:07:55
+Temporary commit content for 2022-10-15T16:07:55

a9834ca8d6333ac3f2793abdd58cb93a4cbec797
Andreiij <andrei.gher@yahoo.com>
2023-04-29 16:07:55 +0100

    Added validation for user input

--- script.ps1
+++ script.ps1
@@ -29,3 +29,4 @@ Temporary commit content for 2023-03-30T16:07:55
 Temporary commit content for 2023-04-02T16:07:55
 Temporary commit content for 2023-04-03T16:07:55
 Temporary commit content for 2023-04-25T16:07:55
+Temporary commit content for 2023-04-29T16:07:55

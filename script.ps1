7b0d98a6eeb61dade9e2885f406cd70a09069151
Andreiij <andrei.gher@yahoo.com>
2023-07-22 16:07:55 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -34,3 +34,4 @@ Temporary commit content for 2023-05-22T16:07:55
 Temporary commit content for 2023-06-07T16:07:55
 Temporary commit content for 2023-06-24T16:07:55
 Temporary commit content for 2023-06-30T16:07:55
+Temporary commit content for 2023-07-22T16:07:55

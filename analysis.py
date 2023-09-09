e91af87317cd0a26b6d2f0a275fd486b806526fa
Andreiij <andrei.gher@yahoo.com>
2023-09-09 16:07:55 +0100

    Fixed typos in documentation

--- analysis.py
+++ analysis.py
@@ -32,3 +32,4 @@ Temporary commit content for 2023-07-17T16:07:55
 Temporary commit content for 2023-08-04T16:07:55
 Temporary commit content for 2023-08-12T16:07:55
 Temporary commit content for 2023-09-06T16:07:55
+Temporary commit content for 2023-09-09T16:07:55

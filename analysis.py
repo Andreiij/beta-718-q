cdfb64d16586af8769669b892293ece8dbfaa801
Andreiij <andrei.gher@yahoo.com>
2024-09-10 16:07:55 +0100

    Fixed bug in data processing script

--- analysis.py
+++ analysis.py
@@ -61,3 +61,4 @@ Temporary commit content for 2024-07-17T16:07:55
 Temporary commit content for 2024-07-19T16:07:55
 Temporary commit content for 2024-07-25T16:07:55
 Temporary commit content for 2024-08-06T16:07:55
+Temporary commit content for 2024-09-10T16:07:55

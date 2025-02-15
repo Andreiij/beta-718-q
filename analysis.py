03282c67475fa670ebb44afd607ee269d2914464
Andreiij <andrei.gher@yahoo.com>
2025-02-15 16:07:55 +0100

    Improved error handling logic

--- analysis.py
+++ analysis.py
@@ -74,3 +74,4 @@ Temporary commit content for 2024-12-13T16:07:55
 Temporary commit content for 2025-01-10T16:07:55
 Temporary commit content for 2025-01-30T16:07:55
 Temporary commit content for 2025-02-06T16:07:55
+Temporary commit content for 2025-02-15T16:07:55
